084bbf5501208c2f471a45ce8f0c5c8ba7036389
golmaker <iv7.software@gmail.com>
2021-11-13 11:53:51 -0300

    first commit contracts gol-farm

---
+++ contracts/libs/MockBEP20.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: UNLICENSED
+/*
+ * GolFinance 
+ * App:             https://gol.finance
+ * Web:             https://golfinance.io
+ * Docs:            https://golfinance.gitbook.io/golfinance/
+ * Twitter:         https://twitter.com/FinanceGol
+ * Telegram:        https://t.me/golfinance
+ * GitHub:          https://github.com/GolFinance
+ */
+pragma solidity 0.6.12;
+
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+
+contract MockBEP20 is BEP20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 supply
+    ) public BEP20(name, symbol) {
+        _mint(msg.sender, supply);
+
+    }
+}
